745892e2b18a12a077272263a24e626e9b55edde
k1000 <selwak@gmail.com>
2011-07-18 11:38:23 -0700

    lowered priority otherwise everything would be urgent by default :D

--- tracker/models.py
+++ tracker/models.py
@@ -18,7 +18,7 @@ class Ticket(models.Model):
     assigned_to = models.ForeignKey(User, verbose_name=_("asignado"), limit_choices_to = {'is_staff__exact': True})
     description = models.TextField(_(u"descripción"), blank=True, null=True)
     status = models.PositiveIntegerField(_("status"), default=1, choices=STATUS_CODES)
-    priority = models.PositiveIntegerField(_("prioridad"), default=1, choices=PRIORITY_CODES)
+    priority = models.PositiveIntegerField(_("prioridad"), default=2, choices=PRIORITY_CODES)
     kind = models.PositiveIntegerField(_("tipo"), default=1, choices=KIND_CODES)
     commit_id = models.CharField(_(u'revisión nº'), blank=True, null=True, max_length=100)
     image = models.ImageField(_("imagen"), blank=True, null=True, upload_to="uploads/tracker", )
