f6e025916d0fc50db3c0bca86b2a5b330bf0ee4f
kamil <selwak@gmail.com>
2011-07-20 14:03:55 +0100

    correct site in ticket notification

--- tracker/notify.py
+++ tracker/notify.py
@@ -7,17 +7,17 @@ from django.contrib.sites.models import Site
 from tracker.settings import NOTIFY_MANAGERS, NOTIFY_FROM_EMAIL
 
 def notify_staff( obj ):
-    import ipdb; ipdb.set_trace()
+
     site = getattr(obj, "sites", None)
     if not site:
-        site = Site.objects.get_current().name
+        site = Site.objects.get_current()
     data = {
-        "site": site,
+        "site": site.name,
         "admin_url": reverse( 'admin:tracker_ticket_change', args=(obj.id,) ),
         'object': obj,
     }
     
-    subject = 'ticket %s: "%s" en %s' % (obj.id, obj.title, data.site)
+    subject = 'ticket %s: "%s" en %s' % (obj.id, obj.title, data["site"])
     message = render_to_string("tracker/email/notify_staff.txt", dictionary=data )
     
     #notify asigned staff member                  
