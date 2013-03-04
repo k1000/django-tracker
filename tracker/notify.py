59e6ea9df572b380b54679e57d72008afdf43f3b
k1000 <selwak@gmail.com>
2013-03-04 21:38:50 +0000

    PEP8 cleaning

--- tracker/notify.py
+++ tracker/notify.py
@@ -6,35 +6,36 @@ from django.contrib.sites.models import Site
 
 from tracker.settings import NOTIFY_MANAGERS, NOTIFY_FROM_EMAIL
 
-def notify_staff( obj ):
+
+def notify_staff(obj):
 
     site = getattr(obj, "sites", None)
     if not site:
         site = Site.objects.get_current()
     data = {
         "site": site.name,
-        "admin_url": reverse( 'admin:tracker_ticket_change', args=(obj.id,) ),
+        "admin_url": reverse('admin:tracker_ticket_change', args=(obj.id,)),
         'object': obj,
     }
-    
+
     subject = 'ticket %s: "%s" en %s' % (obj.id, obj.title, data["site"])
-    message = render_to_string("tracker/email/notify_staff.txt", dictionary=data )
-    
-    #notify asigned staff member                  
+    message = render_to_string("tracker/email/notify_staff.txt", dictionary=data)
+
+    #notify asigned staff member
     send_mail(
-        subject, 
-        message, 
+        subject,
+        message,
         NOTIFY_FROM_EMAIL,
-        NOTIFY_MANAGERS + [obj.assigned_to.email], 
+        NOTIFY_MANAGERS + [obj.assigned_to.email],
         fail_silently=False
     )
-    
+
     if obj.assigned_to_id is not obj.submitter_id:
-        #notify submitter                 
+        #notify submitter
         send_mail(
-            subject, 
-            message, 
+            subject,
+            message,
             NOTIFY_FROM_EMAIL,
-            [obj.submitter.email], 
+            [obj.submitter.email],
             fail_silently=False
         )
