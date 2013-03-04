291200aae5349345f6398b4df9e4fdc6efc7f55d
k1000 <selwak@gmail.com>
2013-03-04 21:46:46 +0000

    PEP8 cleaning

--- tracker/management/commands/git-close-tickets.py
+++ tracker/management/commands/git-close-tickets.py
@@ -10,36 +10,35 @@ logger = logging.getLogger("django")
 
 #based on http://progit.org/book/ch7-4.html
 
+
 class Command(BaseCommand):
     args = '<rev oldrev newrev>'
     help = '''busca ticket refs en git commits y lo cierra'''
-    
+
     def handle(self, *args, **options):
         ref = args[0]
         oldrev = args[1]
         newrev = args[2]
-        self.check_messages( oldrev, newrev )
-
+        self.check_messages(oldrev, newrev)
 
     def check_messages(self, oldrev, newrev):
-        regex= re.compile("#(\d+)")
+        regex = re.compile("#(\d+)")
 
-        missed_revs = commands.getoutput('git rev-list %s..%s' % (oldrev, newrev )).split("\n")
+        missed_revs = commands.getoutput('git rev-list %s..%s' % (oldrev, newrev)).split("\n")
         for rev in missed_revs:
-            message = commands.getoutput("git cat-file commit %s | sed '1,/^$/d'" % rev )
+            message = commands.getoutput("git cat-file commit %s | sed '1,/^$/d'" % rev)
             tickets = regex.findall(message)
             if tickets:
-                self.close_tickets( rev, tickets )
+                self.close_tickets(rev, tickets)
 
+    def close_tickets(self, rev, tickets):
 
-    def close_tickets(self, rev, tickets ):
-        
         for ticket_id in tickets:
             try:
                 ticket = Ticket.objects.get(pk=int(ticket_id))
             except Ticket.DoesNotExist:
-                raise CommandError( "Can't close ticket. Ticket #%s doesn't exist" % ticket_id  )
-                
+                raise CommandError("Can't close ticket. Ticket #%s doesn't exist" % ticket_id)
+
             ticket.status = 3
             ticket.commit_id = rev
             ticket.save()
