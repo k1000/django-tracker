4d2f815622d845c1068515312584cf4dff95cfe7
k1000 <selwak@gmail.com>
2013-03-04 21:42:13 +0000

    PEP8 cleaning

--- tracker/forms.py
+++ tracker/forms.py
@@ -3,11 +3,11 @@ from django import forms
 
 from models import Note
 
+
 class CreateNoteForm(forms.ModelForm):
     class Meta():
         model = Note
         exclude = ("created_at", "created_by",)
-        widgets ={
-            "ticket":forms.HiddenInput(),
+        widgets = {
+            "ticket": forms.HiddenInput(),
         }
-    
